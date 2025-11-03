3f8fc778dceb162c0fd3e7170f7fd983fadbccc1
tohidlo <amir@tohidlo.comgit>
2025-11-03 19:58:39 +0330

    Real-time license check, disable plugin and prevent execution of other code (security 7/10)

--- myplugin.php
+++ myplugin.php
@@ -3,16 +3,26 @@
 Plugin Name: myplugin
 */
 
-register_activation_hook(__FILE__, function() {
+if (!defined('ABSPATH')) exit;
+
+if (!(function() {
     $server_api = 'http://localhost/server-api.php';
     $current_domain = parse_url(home_url(), PHP_URL_HOST);
 
-    $response = wp_remote_get($server_api . '?domain=' . $current_domain);
+    $response = wp_remote_get($server_api . '?domain=' . $current_domain, ['timeout' => 5]);
     $body = wp_remote_retrieve_body($response);
     $data = json_decode($body, true);
-
     if (empty($data['status']) || $data['status'] !== 'ok') {
-        deactivate_plugins(plugin_basename(__FILE__));
-        wp_die('License invalid: this plugin is not allowed on this domain.');
+        return false;
     }
-});
+
+    return true;
+})()) {
+    deactivate_plugins(plugin_basename(__FILE__));
+    add_action('admin_notices', function() {
+        if (current_user_can('manage_options')) {
+            echo '<div class="notice notice-error"><p>myplugin: License not active</p></div>';
+        }
+    });
+    return;
+}
